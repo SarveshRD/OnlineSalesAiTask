fcfdb1ef57d9f55b0693fb6ce8de2e1e069ff241
SarveshRD <sardhaw007@gmail.com>
2023-09-05 23:38:03 +0530

    feat remove

--- app/src/test/java/com/example/onlinesalestask/ExampleUnitTest.kt
+++ app/src/test/java/com/example/onlinesalestask/ExampleUnitTest.kt
@@ -13,7 +13,6 @@ import org.mockito.Mockito.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
 
